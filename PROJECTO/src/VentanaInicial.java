584ee7b1cc64ab9ef47940f29d9ec7d31da67359
Markel <Markel@10.160.15.117>
2022-10-24 12:19:07 +0200

    hol

--- PROJECTO/src/VentanaInicial.java
+++ PROJECTO/src/VentanaInicial.java
@@ -15,7 +15,7 @@ public class VentanaInicial {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		System.out.println("hola");
+		System.out.println(" hola");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
